2ffae2e920d8f38235dce546fb603b8c0fb8570e
InoyBobr <112200763+InoyBobr@users.noreply.github.com>
2026-01-25 05:15:28 +0300

    Update Program.cs

--- GameServer/Program.cs
+++ GameServer/Program.cs
@@ -1,4 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
+long totalWsRequests = 0;
+long acceptedWsRequests = 0;
 var app = builder.Build();
 
 app.UseWebSockets();
@@ -7,14 +9,29 @@ var matchmaker = new Matchmaker();
 
 app.Map("/ws", async context =>
 {
+    Interlocked.Increment(ref totalWsRequests);
+
     if (!context.WebSockets.IsWebSocketRequest)
     {
         context.Response.StatusCode = 400;
         return;
     }
 
+    Interlocked.Increment(ref acceptedWsRequests);
+
     var ws = await context.WebSockets.AcceptWebSocketAsync();
     await matchmaker.HandleClient(ws);
 });
 
+app.MapGet("/stats", () =>
+{
+    return new
+    {
+        totalWsRequests,
+        acceptedWsRequests,
+        time = DateTime.UtcNow
+    };
+});
+
 app.Run();
+
