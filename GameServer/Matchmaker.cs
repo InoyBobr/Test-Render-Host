0b0915d9603594521dffdbfd0f8b1ee90f38d1c7
InoyBobr <112200763+InoyBobr@users.noreply.github.com>
2026-01-26 05:38:49 +0300

    Update Matchmaker.cs

--- GameServer/Matchmaker.cs
+++ GameServer/Matchmaker.cs
@@ -4,25 +4,55 @@ public class Matchmaker
 {
     private Connection? waitingPlayer;
     private readonly List<Session> sessions = new();
+    private readonly object sync = new();
 
     public async Task HandleClient(WebSocket ws)
     {
-        var connection = new Connection(ws);
+        var connection = new Connection(ws, OnConnectionDead);
 
-        if (waitingPlayer == null)
+        lock (sync)
         {
-            waitingPlayer = connection;
-            await connection.Send(new { type = "waiting" });
+            if (waitingPlayer == null || waitingPlayer.IsDead)
+            {
+                waitingPlayer = connection;
+            }
+            else
+            {
+                var session = new Session(
+                    waitingPlayer,
+                    connection,
+                    OnSessionEnded
+                );
+
+                sessions.Add(session);
+                waitingPlayer = null;
+
+                _ = session.Start();
+            }
         }
-        else
-        {
-            var session = new Session(waitingPlayer, connection);
-            sessions.Add(session);
-            waitingPlayer = null;
 
-            await session.Start();
+        if (connection.Session == null)
+        {
+            await connection.Send(new { type = "waiting" });
         }
 
         await connection.Listen();
+    }
+
+    private void OnSessionEnded(Session session)
+    {
+        lock (sync)
+        {
+            sessions.Remove(session);
+        }
+    }
+
+    private void OnConnectionDead(Connection c)
+    {
+        lock (sync)
+        {
+            if (waitingPlayer == c)
+                waitingPlayer = null;
+        }
     }
 }
