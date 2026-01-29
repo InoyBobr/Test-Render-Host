c4def20ab78f2e17de64d64fcd5feec350f4ddd1
InoyBobr <112200763+InoyBobr@users.noreply.github.com>
2026-01-29 07:01:00 +0300

    Create ConnectionPair.cs

---
+++ GameServer/ConnectionPair.cs
@@ -0,0 +1,72 @@
+public class ConnectionPair
+{
+    private readonly Connection a;
+    private readonly Connection b;
+
+    private bool aConfirmed;
+    private bool bConfirmed;
+    private bool isBroken;
+
+    public event Action<ConnectionPair>? OnConfirmed;
+    public event Action<ConnectionPair>? OnBroken;
+
+    public ConnectionPair(Connection a, Connection b)
+    {
+        this.a = a;
+        this.b = b;
+
+        a.OnHeartbeat += OnHeartbeat;
+        b.OnHeartbeat += OnHeartbeat;
+
+        a.OnDead += OnDead;
+        b.OnDead += OnDead;
+
+        a.OnSleep += OnSleep;
+        b.OnSleep += OnSleep;
+    }
+
+    private void OnHeartbeat(Connection c)
+    {
+        if (isBroken) return;
+
+        if (c == a) aConfirmed = true;
+        if (c == b) bConfirmed = true;
+
+        if (aConfirmed && bConfirmed)
+        {
+            Cleanup();
+            OnConfirmed?.Invoke(this);
+        }
+    }
+
+    private void OnDead(Connection _)
+    {
+        Break();
+    }
+
+    private void OnSleep(Connection _)
+    {
+        Break();
+    }
+
+    private void Break()
+    {
+        if (isBroken) return;
+        isBroken = true;
+
+        Cleanup();
+        OnBroken?.Invoke(this);
+    }
+
+    private void Cleanup()
+    {
+        a.OnHeartbeat -= OnHeartbeat;
+        b.OnHeartbeat -= OnHeartbeat;
+        a.OnDead -= OnDead;
+        b.OnDead -= OnDead;
+        a.OnSleep -= OnSleep;
+        b.OnSleep -= OnSleep;
+    }
+
+    public (Connection, Connection) GetConnections() => (a, b);
+}
