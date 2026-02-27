2bf89cd3cc29a537961bb9795741be7dc2f56867
InoyBobr <112200763+InoyBobr@users.noreply.github.com>
2026-02-27 03:10:57 +0300

    Update Program.cs

--- GameServer/Program.cs
+++ GameServer/Program.cs
@@ -4,6 +4,28 @@ long acceptedWsRequests = 0;
 long statRequests = 0;
 var app = builder.Build();
 
+var json = @"
+{
+  ""CardId"": ""fire_elemental"",
+  ""basePower"": 3,
+  ""baseHealth"": 5,
+  ""color"": ""Red"",
+  ""keywords"": [""Charge"", ""Burn""],
+  ""abilities"": [
+    {
+      ""AbilityId"": ""DealDamage"",
+      ""Parameters"": [
+        { ""key"": ""amount"", ""value"": 3 }
+      ]
+    }
+  ]
+}";
+
+var data = CardDataLoader.FromJson(json);
+Console.WriteLine(data.basePower);
+Console.WriteLine(data.keywords.Count);
+Console.WriteLine(data.baseHealth);
+
 app.UseWebSockets();
 
 var matchmaker = new Matchmaker();
@@ -32,36 +54,18 @@ app.MapGet("/stats", () =>
         statRequests,
         totalWsRequests,
         acceptedWsRequests,
+        data.basePower,
+        data.keywords.Count,
+        data.baseHealth,
         time = DateTime.UtcNow
     };
 });
 
 app.Run();
 
-var json = @"
-{
-  ""CardId"": ""fire_elemental"",
-  ""basePower"": 3,
-  ""baseHealth"": 5,
-  ""color"": ""Red"",
-  ""keywords"": [""Charge"", ""Burn""],
-  ""abilities"": [
-    {
-      ""AbilityId"": ""DealDamage"",
-      ""Parameters"": [
-        { ""key"": ""amount"", ""value"": 3 }
-      ]
-    }
-  ]
-}";
-
-var data = CardDataLoader.FromJson(json);
-Console.WriteLine(data.basePower);
-Console.WriteLine(data.keywords.Count);
-Console.WriteLine(data.baseHealth);
-
 
     
+
 
 
 
