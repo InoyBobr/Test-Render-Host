5fbc82bb7f963e36972c22d88925af4f9b816e2b
InoyBobr <ivlenkov.t@gmail.com>
2026-06-03 19:42:50 +0300

    Balance update, bug fixed

--- GameServer/Abilities/Damage All enemies after Death.cs
+++ GameServer/Abilities/Damage All enemies after Death.cs
@@ -22,7 +22,13 @@ public class Damage_All_enemies_after_Death : AbilityLogic
             return;
         if (!State.IntValues.TryGetValue("damage", out var damage))
             return;
-        var selector = new TargetSelector(TargetSide.Enemy, CardZone.Board, FaceConstraint.Any, StatConstraint.Any, TargetPick.All);
-        Bus.Publish(new RandomCardDamageRequestEvent(selector, damage, Owner));
+        //var selector = new TargetSelector(TargetSide.Enemy, CardZone.Board, FaceConstraint.Any, StatConstraint.Any, TargetPick.All);
+        var ctx = Owner._api.GetContext(Owner.Owner);
+        var enemies = ctx.GetEnemyCardsOnFace(OwnerUnit);
+        foreach (var enemy in enemies)
+        {
+            Bus.Publish(new CardNonCombatDamageRequestEvent(enemy, damage, Owner));
+        }
+        //Bus.Publish(new RandomCardDamageRequestEvent(selector, damage, Owner));
     }
 }
